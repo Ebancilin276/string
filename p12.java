250a607f907e4b6d6cb582e90811428b5a07039f
Ebancilin276 <24ucs130ebancilinj@kgkite.ac.in>
2026-09-25 20:04:03 +0530

    Add palindrome checking program

---
+++ p12.java
@@ -0,0 +1,24 @@
+public class p12 {
+    public static void main(String[] args) {
+
+        String s = "madam";
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        boolean palindrome = true;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                palindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        System.out.println(palindrome);
+    }
+}
