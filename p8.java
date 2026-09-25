b7681847e2d26fbee1d9872de79b9990afc2c54f
Ebancilin276 <24ucs130ebancilinj@kgkite.ac.in>
2026-09-25 19:55:44 +0530

    Add uppercase to lowercase program

---
+++ p8.java
@@ -0,0 +1,20 @@
+public class p8 {
+    public static void main(String[] args) {
+
+        String s = "HELLO";
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+
+            result += ch;
+        }
+
+        System.out.println(result);
+    }
+}
